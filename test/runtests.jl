e4a7f4bfa6efe93e8cd0c4519130e4d0500e2fba
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2022-07-06 10:50:32 +0200

    Make runtests.jl debuggable

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Test
 using NonLinearSystemNeuralNetworkFMU
 
+include("profiling_tests.jl")
+include("gen_fmus_test.jl")
+
 path_to_omc = ""
 if Sys.iswindows()
   path_to_omc = string(strip(read(`where omc.exe`, String)))
@@ -15,5 +18,5 @@ end
 
 path_to_fmi_header = abspath(joinpath(@__DIR__, "..", "FMI-Standard-2.0.3","headers"))
 
-include("profiling_tests.jl")
-include("gen_fmus_test.jl")
+runProfilingTests()
+runGenFmusTest()
