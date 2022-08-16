69cd657926df3712c343cd2d3a74427f4f9b475b
Andreas <38031952+AnHeuermann@users.noreply.github.com>
2022-08-16 16:01:28 +0200

    Replace MIT license with GPLv3 license (#22)

--- src/onnxWrapper/onnxWrapper.h
+++ src/onnxWrapper/onnxWrapper.h
@@ -1,7 +1,20 @@
 //
 // Copyright (c) 2022 Andreas Heuermann
-// Licensed under the MIT license. See LICENSE.md file in the
-// NonLinearSystemNeuralNetworkFMU.jl root for details.
+//
+// This file is part of NonLinearSystemNeuralNetworkFMU.jl.
+//
+// NonLinearSystemNeuralNetworkFMU.jl is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// NonLinearSystemNeuralNetworkFMU.jl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with NonLinearSystemNeuralNetworkFMU.jl. If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "onnxruntime_c_api.h"
