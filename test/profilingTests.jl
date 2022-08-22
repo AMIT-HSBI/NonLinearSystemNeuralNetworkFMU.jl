b1a4a6c1fce3f845433766e00ce63565c88196fb
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2022-08-22 09:32:33 +0200

    Fixing merge issues

--- test/profilingTests.jl
+++ test/profilingTests.jl
@@ -38,29 +38,28 @@ function runProfilingTests()
   pathToMo = abspath(@__DIR__,"simpleLoop.mo")
   workingDir = abspath(@__DIR__)
 
-    @testset "Find slowes equations" begin
-      profilingInfo = NonLinearSystemNeuralNetworkFMU.profiling(modelName, pathToMo, pathToOmc; workingDir=workingDir, threshold=0)
-      @test length(profilingInfo) == 2
-      # NLS from simulation system
-      @test profilingInfo[1].eqInfo.id == 14
-      @test profilingInfo[1].iterationVariables == ["y"]
-      @test sort(profilingInfo[1].usingVars) == ["r","s"]
-      # NLS from initialization system
-      @test profilingInfo[2].eqInfo.id == 6
-      @test profilingInfo[2].iterationVariables == ["y"]
-      @test sort(profilingInfo[2].usingVars) == ["r","s"]
-      rm(joinpath(workingDir,modelName), recursive=true)
-    end
+  @testset "Find slowes equations" begin
+    profilingInfo = NonLinearSystemNeuralNetworkFMU.profiling(modelName, pathToMo, pathToOmc; workingDir=workingDir, threshold=0)
+    @test length(profilingInfo) == 2
+    # NLS from simulation system
+    @test profilingInfo[1].eqInfo.id == 14
+    @test profilingInfo[1].iterationVariables == ["y"]
+    @test sort(profilingInfo[1].usingVars) == ["r","s"]
+    # NLS from initialization system
+    @test profilingInfo[2].eqInfo.id == 6
+    @test profilingInfo[2].iterationVariables == ["y"]
+    @test sort(profilingInfo[2].usingVars) == ["r","s"]
+    rm(joinpath(workingDir,modelName), recursive=true)
+  end
 
-    @testset "Min-max for usingVars" begin
-      profilingInfo = NonLinearSystemNeuralNetworkFMU.profiling(modelName, pathToMo, pathToOmc; workingDir=workingDir, threshold=0)
-      (min, max)  = NonLinearSystemNeuralNetworkFMU.minMaxValuesReSim(profilingInfo[1].usingVars, modelName, pathToMo, pathToOmc; workingDir=workingDir)
-      # s = sqrt((2-time)*0.9), time = 0..2
-      # r = 1..3
-      @test min[1] ≈ 0.0 && max[1] ≈ 1.4087228258248679
-      @test min[2] ≈ 0.95 && max[2] ≈ 3.15
-      rm(joinpath(workingDir,modelName), recursive=true)
-    end
+  @testset "Min-max for usingVars" begin
+    profilingInfo = NonLinearSystemNeuralNetworkFMU.profiling(modelName, pathToMo, pathToOmc; workingDir=workingDir, threshold=0)
+    (min, max)  = NonLinearSystemNeuralNetworkFMU.minMaxValuesReSim(profilingInfo[1].usingVars, modelName, pathToMo, pathToOmc; workingDir=workingDir)
+    # s = sqrt((2-time)*0.9), time = 0..2
+    # r = 1..3
+    @test min[1] ≈ 0.0 && max[1] ≈ 1.4087228258248679
+    @test min[2] ≈ 0.95 && max[2] ≈ 3.15
+    rm(joinpath(workingDir,modelName), recursive=true)
   end
 end
 
