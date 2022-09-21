79cf307d4cdab49e0fcda93bfa26ed9a795eafe7
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2022-09-21 11:22:14 +0200

    Updating example

--- example/ScalableTranslationStatistics.jl
+++ example/ScalableTranslationStatistics.jl
@@ -1,14 +1,22 @@
-# $julia -e "include(\"ScalableTranslationStatistics.jl\"); run()"
-# $nohup julia-1.8.1 -e "include(\"ScalableTranslationStatistics.jl\"); run()" &
+# $julia -e "include(\"ScalableTranslationStatistics.jl\"); genFMUs()"
+# $nohup julia-1.8.1 -e "include(\"ScalableTranslationStatistics.jl\"); genFMUs()" &
 
 cd(@__DIR__)
 using NaiveONNX
 import Pkg; Pkg.activate("..")
 using NonLinearSystemNeuralNetworkFMU
 using BSON: @save, @load
+using FMI
+using FMICore
+using CSV
+using DataFrames
+using BenchmarkTools
+using Plots
 
 ENV["ORT_DIR"] = "/mnt/home/aheuermann/workdir/julia/NonLinearSystemNeuralNetworkFMU.jl/onnxruntime-linux-x64-1.12.1"
 rootDir = "/mnt/home/aheuermann/workdir/phymos/ScalableTranslationStatistics"
+
+sizes = ["5", "10", "20", "40", "80", "160"]
 
 function runScalableTranslationStatistics(rootDir::String; level::Integer=1, N::Integer=10000)
   # Get lib and model
@@ -16,8 +24,7 @@ function runScalableTranslationStatistics(rootDir::String; level::Integer=1, N::
   if !isfile(lib)
     @error "Could not find Modelica library at $(lib)"
   end
-  size = ["5", "10", "20", "40", "80", "100"]
-  modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(size[level])"
+  modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
 
   # Profile non-linear loops
   @showtime profilingInfo = profiling(modelName, lib; threshold=0)
@@ -91,6 +98,30 @@ function runScalableTranslationStatistics(rootDir::String; level::Integer=1, N::
   return (profilingInfo, fmu, fmu_onnx)
 end
 
+function DataFrame(result::FMICore.FMU2Solution{FMU2}, names::Array{String})
+  df = DataFrames.DataFrame(time=result.values.t)
+  for i in 1:length(result.values.saveval[1])
+    df[!, Symbol(names[i])] = [val[i] for val in result.values.saveval]
+  end
+  return df
+end
+
+function simulateFMU(pathToFMU, resultFile)
+  mkpath(dirname(resultFile))
+
+  outputVars = ["outputs[1]", "outputs[2]", "outputs[3]", "outputs[4]", "outputs[5]", "outputs[6]", "outputs[7]", "outputs[8]"]
+
+  fmu = fmiLoad(pathToFMU)
+  result = fmiSimulate(fmu, 0.0, 10.0; recordValues=outputVars)
+  df = DataFrame(result, outputVars)
+
+  bench = @benchmark fmiSimulate($fmu, 0.0, 10.0; recordValues=$outputVars) evals=1 samples=1
+  fmiUnload(fmu)
+
+  CSV.write(resultFile, df)
+  return bench
+end
+
 function clean()
   rm("data/", recursive=true, force=true)
   rm("fmus/", recursive=true, force=true)
@@ -105,11 +136,48 @@ function clean()
 end
 
 # Create all the FMUs
-function run()
-  for level in 1:6
+function genFMUs()
+  for level in 6:6
     @info "Starting level $level"
     redirect_stdio(stdout="level$(string(level)).log", stderr="level$(string(level)).log") do
       @time runScalableTranslationStatistics(rootDir, level = level)
     end
   end
+end
+
+function runBenchmarks(levels=1:6)
+  for level in levels
+    @info "Benchmarking level $level"
+    resultDir = joinpath(@__DIR__, "results", "level$(string(level))")
+    modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
+    defaultFMU = joinpath(@__DIR__, "fmus", "level$(string(level))", modelName*".fmu")
+    onnxFMU = joinpath(@__DIR__, "fmus", "level$(string(level))", modelName*".onnx.fmu")
+    defaultResult = joinpath(resultDir, modelName*".csv")
+    onnxResult = joinpath(resultDir, modelName*".onnx.csv")
+    redirect_stdio(stdout="bench_level$(string(level)).log", stderr="level$(string(level)).log") do
+      time_default = simulateFMU(defaultFMU, defaultResult)
+      time_onnx = simulateFMU(onnxFMU, onnxResult)
+      @save joinpath(resultDir, "benchTimes.bson") time_default time_onnx
+    end
+  end
+end
+
+
+function plotResult(;level=1, outputs=1:8)
+  resultDir = joinpath(@__DIR__, "results", "level$(string(level))")
+  modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
+  defaultResult = joinpath(resultDir, modelName*".csv")
+  onnxResult = joinpath(resultDir, modelName*".onnx.csv")
+
+  df_def = DataFrames.DataFrame(CSV.File(defaultResult))
+  df_onnx = DataFrames.DataFrame(CSV.File(onnxResult))
+
+  p = plot(title = "level $level", legend=:topleft)
+  for (i,out) in enumerate(outputs)
+    name = "outputs[$out]"
+    p = plot(p, df_def.time, df_def[!,Symbol(name)], label=name*" ref", color=i)
+    p = plot(p, df_onnx.time, df_onnx[!,Symbol(name)], label=name*" onnx", color=i, linestyle=:dash)
+  end
+
+  return p
 end
