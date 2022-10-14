9aee7eb4e890a41f255dac399f21466619770119
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2022-10-14 13:59:52 +0200

    Move ploting functions into separate file

--- examples/ScalableTranslationStatistics/ScalableTranslationStatistics.jl
+++ examples/ScalableTranslationStatistics/ScalableTranslationStatistics.jl
@@ -1,17 +1,16 @@
 # $ julia -e "include(\"ScalableTranslationStatistics.jl\"); genFMUs()"
-# $ nohup julia-1.8.1 -e "include(\"ScalableTranslationStatistics.jl\"); genFMUs()" &
+# $ nohup julia -e "include(\"ScalableTranslationStatistics.jl\"); genFMUs()" &
 
 cd(@__DIR__)
 using NaiveONNX
 import Pkg; Pkg.activate("../..")
 using NonLinearSystemNeuralNetworkFMU
-using BSON: @save, @load
+using BSON: @save
 using FMI
 using FMICore
 using CSV
 using DataFrames
 using BenchmarkTools
-using Plots
 
 ENV["ORT_DIR"] = "/mnt/home/aheuermann/workdir/julia/NonLinearSystemNeuralNetworkFMU.jl/onnxruntime-linux-x64-1.12.1"
 rootDir = "/mnt/home/aheuermann/workdir/phymos/ScalableTranslationStatistics"
@@ -69,24 +68,11 @@ function simulateFMU(pathToFMU, resultFile)
   result = fmiSimulate(fmu, 0.0, 10.0; recordValues=outputVars)
   df = DataFrame(result, outputVars)
 
-  bench = @benchmark fmiSimulate($fmu, 0.0, 10.0; recordValues=$outputVars)
+  bench = @benchmark fmiSimulate($fmu, 0.0, 10.0; recordValues=$outputVars) samples=5 seconds=120*5 evals=1
   fmiUnload(fmu)
 
   CSV.write(resultFile, df)
   return bench
-end
-
-function clean()
-  rm("data/", recursive=true, force=true)
-  rm("fmus/", recursive=true, force=true)
-  rm("onnx/", recursive=true, force=true)
-  rm("profiling/", recursive=true, force=true)
-  foreach(rm, filter(endswith(".log"), readdir(@__DIR__,join=true)))
-  for dir in readdir(@__DIR__)
-    if startswith(dir, "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_")
-      rm(dir, recursive=true)
-    end
-  end
 end
 
 # Create all the FMUs
@@ -105,75 +91,16 @@ function runBenchmarks(levels=1:6)
     @info "Benchmarking level $level"
     resultDir = joinpath(@__DIR__, "results", "level$(string(level))")
     modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
-    defaultFMU = joinpath(@__DIR__, "fmus", "level$(string(level))", modelName*".fmu")
-    onnxFMU = joinpath(@__DIR__, "fmus", "level$(string(level))", modelName*".onnx.fmu")
+    defaultFMU = joinpath(@__DIR__, "level$(string(level))", modelName*".fmu")
+    onnxFMU = joinpath(@__DIR__, "level$(string(level))", modelName*".onnx.fmu")
     defaultResult = joinpath(resultDir, modelName*".csv")
     onnxResult = joinpath(resultDir, modelName*".onnx.csv")
     redirect_stdio(stdout="bench_level$(string(level)).log", stderr="level$(string(level)).log") do
       time_default = simulateFMU(defaultFMU, defaultResult)
+      @show time_default
       time_onnx = simulateFMU(onnxFMU, onnxResult)
+      @show time_onnx
       @save joinpath(resultDir, "benchTimes.bson") time_default time_onnx
     end
   end
-end
-
-# Plot FMU simulations
-function plotResult(;level=1, outputs=1:8, tspan=nothing)
-  resultDir = joinpath(@__DIR__, "results", "level$(string(level))")
-  modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
-  defaultResult = joinpath(resultDir, modelName*".csv")
-  onnxResult = joinpath(resultDir, modelName*".onnx.csv")
-
-  df_def = DataFrames.DataFrame(CSV.File(defaultResult))
-  df_onnx = DataFrames.DataFrame(CSV.File(onnxResult))
-
-  if tspan !== nothing
-    i_start = findfirst(x->x>= tspan[1], df_def.time)
-    i_end = findnext(x->x>= tspan[end], df_def.time, i_start)
-    df_def = df_def[i_start:i_end,:]
-    i_start = findfirst(x->x>= tspan[1], df_onnx.time)
-    i_end = findnext(x->x>= tspan[end], df_onnx.time, i_start)
-    df_onnx = df_onnx[i_start:i_end,:]
-  end
-
-  p = plot(title = "ScaledNLEquations.NLEquations_$(sizes[level])", legend=:topleft, xlabel="time [s]")
-  for (i,out) in enumerate(outputs)
-    name = "outputs[$out]"
-    p = plot(p, df_def.time, df_def[!,Symbol(name)], label=name*" ref", color=i)
-    p = plot(p, df_onnx.time, df_onnx[!,Symbol(name)], label=name*" onnx", color=i, linestyle=:dash)
-  end
-
-  return p
-end
-
-
-function animateData(;level=4, eq_idx=1, i_out=1, angle=40)
-  modelName = "ScalableTranslationStatistics.Examples.ScaledNLEquations.NLEquations_$(sizes[level])"
-
-  @load joinpath("profiling", "level$(string(level))", "profilingInfo.bson") profilingInfo
-  prof = profilingInfo[eq_idx]
-
-  dataFile = joinpath(@__DIR__, "data", "level$(string(level))", "eq_$(prof.eqInfo.id).csv")
-  df = DataFrames.DataFrame(CSV.File(dataFile))
-
-  inputNames = prof.usingVars
-  outputNames = prof.iterationVariables
-
-  p = scatter(df[!,Symbol(inputNames[1])],
-              df[!,Symbol(inputNames[2])],
-              df[!,Symbol(outputNames[i_out])],
-              xlabel=inputNames[1], ylabel=inputNames[2], label=[outputNames[i_out]],
-              title="Equation $(prof.eqInfo.id)",
-              markersize=1, markeralpha=0.5, markerstrokewidth=0,
-              color=2,
-              camera = (angle, 30))
-
-  return p
-end
-
-function genGif(level, i_out)
-  anim = @animate for angle in 0:0.5:360
-    animateData(;level=level, eq_idx=1, i_out=i_out, angle=angle)
-  end
-  gif(anim, "data_animation_level$(level)_var$(i_out).gif", fps = 30)
 end
