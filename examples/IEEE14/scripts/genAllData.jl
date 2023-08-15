a116cb8148cb391b3556f7b2c22999286e1d9fe1
Felix Brandt <93375746+dreivmeister@users.noreply.github.com>
2023-08-15 11:22:59 +0200

    updating IEEE14
    updated example to use NonLinearSystemNeuralNetworkFMU.jl version to 0.5.1

--- examples/IEEE14/scripts/genAllData.jl
+++ examples/IEEE14/scripts/genAllData.jl
@@ -6,12 +6,14 @@ using NonLinearSystemNeuralNetworkFMU
 modelName = "IEEE_14_Buses"
 moFiles = [srcdir("IEEE_14_Buses.mo")]
 
-function genData(modelName::String, moFiles::Array{String}, N::Integer)
-  workdir = datadir("sims", "$(modelName)_$(N)")
+function genData(modelName::String, moFiles::Array{String}, n::Integer)
+  workdir = datadir("sims", "$(modelName)_$(n)")
   options = OMOptions(workingDir=workdir,
                       clean=false,
                       commandLineOptions="--preOptModules-=wrapFunctionCalls --postOptModules-=wrapFunctionCalls")
-  main(modelName, moFiles; options=options, reuseArtifacts=true, N=N)
+  dataGenOptions = DataGenOptions(method=RandomMethod(), n=n, nBatches=Threads.nthreads())
+  
+  main(modelName, moFiles; omOptions=options, dataGenOptions=dataGenOptions, reuseArtifacts=true)
 end
 
 (csvFiles, fmu, profilingInfo) = genData(modelName, moFiles, 1000)
