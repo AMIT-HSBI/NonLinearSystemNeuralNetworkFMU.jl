f47b89efc538b6f7793136130ba8cfd6a7ae8654
Felix Brandt <93375746+dreivmeister@users.noreply.github.com>
2023-11-08 10:39:01 +0100

    onnx export doesnt work for multiple outputs

--- examples/IEEE14/scripts/trainGp.py
+++ examples/IEEE14/scripts/trainGp.py
@@ -10,6 +10,13 @@ from sklearn.gaussian_process.kernels import RBF
 
 from clusterData import cluster
 
+import onnx
+import onnxruntime as rt
+import skl2onnx
+from skl2onnx.common.data_types import FloatTensorType, DoubleTensorType
+from skl2onnx import convert_sklearn
+from skl2onnx.helpers.onnx_helper import save_onnx_model, load_onnx_model
+
 # read data
 eg_1403_data = pd.read_csv('/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/scripts/eq_1403_10000.csv')
 inp = eg_1403_data.iloc[:, :16]
@@ -58,8 +65,26 @@ lengthscale = 1.0
 kernel = noise_variance * RBF(lengthscale)
 
 gpr = GaussianProcessRegressor(kernel=kernel, normalize_y=True, random_state=0)
-gpr.fit(X_train_1403_scaled, y_train_1403_scaled)
+gpr.fit(X_train_1403_scaled[:100], y_train_1403_scaled[:100,1])
 
-print('mae on test data', mean_absolute_error(y_test_1403_scaled, gpr.predict(X_test_1403_scaled)))
-print('mse on test', mean_squared_error(y_test_1403_scaled, gpr.predict(X_test_1403_scaled)))
-print('r2 score on test', gpr.score(X_test_1403_scaled, y_test_1403_scaled))
+print('mae on test data', mean_absolute_error(y_test_1403_scaled[:,1], gpr.predict(X_test_1403_scaled)))
+print('mse on test', mean_squared_error(y_test_1403_scaled[:,1], gpr.predict(X_test_1403_scaled)))
+print('r2 score on test', gpr.score(X_test_1403_scaled, y_test_1403_scaled[:,1]))
+
+
+def predict_with_onnxruntime(onx, X):
+    sess = rt.InferenceSession(onx.SerializeToString(), providers=["CPUExecutionProvider"])
+    input_name = sess.get_inputs()[0].name
+    res = sess.run(None, {input_name: X.astype(np.float64)})
+    return res[0]
+
+
+gpr_mixin = skl2onnx.wrap_as_onnx_mixin(gpr, target_opset=12)
+
+onx = gpr_mixin.to_onnx(X_train_1403_scaled.astype(np.float64))
+print(predict_with_onnxruntime(onx, X_test_1403_scaled) - gpr.predict(X_test_1403_scaled))
+
+# # save trained gpr as onnx model using Double instead of float
+# initial_type = [("X_1403", DoubleTensorType([None, None]))] # may be wrong with X_1403
+# onx64 = convert_sklearn(gpr, initial_types=initial_type, target_opset=12)
+# save_onnx_model(onx64, save_path)
