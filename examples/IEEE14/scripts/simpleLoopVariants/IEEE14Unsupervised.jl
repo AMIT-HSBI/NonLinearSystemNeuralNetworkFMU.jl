62a1680e236ad1099b4b30e5231f3f7b9c8716c0
Felix Brandt <93375746+dreivmeister@users.noreply.github.com>
2024-01-11 09:50:08 +0100

    residual training of IEEE14

--- examples/IEEE14/scripts/simpleLoopVariants/IEEE14Unsupervised.jl
+++ examples/IEEE14/scripts/simpleLoopVariants/IEEE14Unsupervised.jl
@@ -28,13 +28,13 @@ include("utils.jl")
 include("trainUnsupervised.jl")
 
 # prepare the data for multiple experiments
-fileName = "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_1000/data/eq_1403.csv"
+fileName = "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_111/data/eq_1403.csv"
 nInputs = 16
 nOutputs = 110
 
-comp, fmu, profilinginfo, vr, row_value_reference, eq_num, sys_num = prepare_fmu("/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_1000/IEEE_14_Buses.interface.fmu",
-                                                            "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_1000/profilingInfo.bson",
-                                                            "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_1000/temp-profiling/IEEE_14_Buses.c")
+comp, fmu, profilinginfo, vr, row_value_reference, eq_num, sys_num = prepare_fmu("/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_111/IEEE_14_Buses.interface.fmu",
+                                                            "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_111/profilingInfo.bson",
+                                                            "/home/fbrandt3/arbeit/NonLinearSystemNeuralNetworkFMU.jl/examples/IEEE14/data/sims/IEEE_14_Buses_111/temp-profiling/IEEE_14_Buses.c")
 
 # 1. some UNAMBIGOUS dataset and train all methods for performance
 # prepare train and test data
@@ -49,6 +49,16 @@ cluster_indices, num_clusters = cluster_data(out_data)
 cluster_index = 1 #rand(1:num_clusters)
 in_data = extract_cluster(in_data, cluster_indices, cluster_index)
 out_data = extract_cluster(out_data, cluster_indices, cluster_index)
+
+# take time out, probably earlier
+in_data = in_data[2:end,:]
+aa = []
+for i in 2:16
+    push!(aa, row_value_reference[i])
+end
+aa = Int64.(aa)
+
+
 # split one cluster into train and test
 train_in, train_out, test_in, test_out = split_train_test(in_data, out_data)
 train_in, train_out, test_in, test_out, train_in_t, train_out_t, test_in_t, test_out_t = scale_data_uniform(train_in, train_out, test_in, test_out)
@@ -56,12 +66,16 @@ dataloader = Flux.DataLoader((train_in, train_out), batchsize=1, shuffle=true)
 
 hidden_width = 100
 model = Flux.Chain(
-  Flux.Dense(nInputs, hidden_width, relu),
+  Flux.Dense(nInputs-1, hidden_width, relu),
   Flux.Dense(hidden_width, hidden_width, relu),
   Flux.Dense(hidden_width, hidden_width, relu),
   Flux.Dense(hidden_width, nOutputs)
 )
 opt = Flux.Adam(1e-4)
+
+
+
+
 
 unsupervised_model, unsupervised_test_loss_hist, res_unsup, unsupervised_time = trainModelUnsupervised(deepcopy(model), 
                                                                                                        deepcopy(opt), 
@@ -74,5 +88,8 @@ unsupervised_model, unsupervised_test_loss_hist, res_unsup, unsupervised_time = 
                                                                                                        test_out_t, 
                                                                                                        eq_num, 
                                                                                                        sys_num, 
-                                                                                                       deepcopy(row_value_reference), 
-                                                                                                       deepcopy(fmu); epochs=100)
+                                                                                                       deepcopy(aa), 
+                                                                                                       deepcopy(fmu); epochs=1000)
+
+
+plot_loss_history(unsupervised_test_loss_hist) # diverges after maybe 100 epochs
