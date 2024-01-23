c794887174548ecd4567e99fba9cf77af00f19f9
Felix Brandt <93375746+dreivmeister@users.noreply.github.com>
2024-01-23 12:44:22 +0100

    added implications

--- examples/IEEE14/scripts/simpleLoopVariants/trainAll.jl
+++ examples/IEEE14/scripts/simpleLoopVariants/trainAll.jl
@@ -43,6 +43,7 @@ comp, fmu, profilinginfo, vr, row_value_reference, eq_num, sys_num = prepare_fmu
 # 1. some UNAMBIGOUS dataset and train all methods for performance
 # prepare train and test data
 (train_in, train_out, test_in, test_out) = readData(fileName, nInputs)
+
 # concat in and out data
 in_data = hcat(train_in, test_in)
 out_data = hcat(train_out, test_out)
@@ -52,20 +53,20 @@ out_data = hcat(x, out_data')'
 # cluster out data
 cluster_indices, num_clusters = cluster_data(out_data)
 # extract cluster
-cluster_index = 1 #rand(1:num_clusters)
+cluster_index = 1
 in_data = extract_cluster(in_data, cluster_indices, cluster_index)
 out_data = extract_cluster(out_data, cluster_indices, cluster_index)
 out_data = out_data[2,:]
 out_data = reshape(out_data, 1, length(out_data))
 # split one cluster into train and test
 train_in, train_out, test_in, test_out = split_train_test(in_data, out_data)
+# min max scaling
 train_in, train_out, test_in, test_out, train_in_t, train_out_t, test_in_t, test_out_t = scale_data_uniform(train_in, train_out, test_in, test_out)
 dataloader = Flux.DataLoader((train_in, train_out), batchsize=1, shuffle=true)
 
 hidden_width = 100
 model = Flux.Chain(
   Flux.Dense(nInputs, hidden_width, relu),
-  Flux.Dense(hidden_width, hidden_width, relu),
   Flux.Dense(hidden_width, hidden_width, relu),
   Flux.Dense(hidden_width, nOutputs)
 )
@@ -84,10 +85,10 @@ title!("MSE for clustered dataset")
 xlabel!("Number of Epochs")
 ylabel!("MSE")
 
-
+# plot residual loss results
 plot_loss_history(res_sup; label="supervised")
 plot_loss_history!(res_unsup; label="unsupervised")
-title!("Residual for clustered dataset")
+title!("Residual loss for clustered dataset")
 xlabel!("Number of Epochs")
 ylabel!("Residual")
 
@@ -104,11 +105,20 @@ xlabel!("x (x=r*s+b-y)")
 ylabel!("y prediction")
 
 
+# ⟹ unsupervised training can converge to a true solution
+# ⟹ purely unsupervised is also unguided (random to which solution the net converges)
+# ⟹ only superior to supervised when clustering is not available: more compute while training, unguided
+# ⟹ semi-supervised is an option, when a preferred solution exists: less data generation, guiding of unsupervised training
+
+# ¡ semi-supervised for now only through loss function, but two-step training procedure is imaginable
+
+
+
 # 2. some AMBIGOUS dataset and train all methods for performance (no clustering)
 # prepare train and test data
 (train_in, train_out, test_in, test_out) = readData(fileName, nInputs)
 train_in, train_out, test_in, test_out, train_in_t, train_out_t, test_in_t, test_out_t = scale_data_uniform(train_in, train_out, test_in, test_out)
-dataloader = Flux.DataLoader((train_in, train_out), batchsize=8, shuffle=true) #???
+dataloader = Flux.DataLoader((train_in, train_out), batchsize=1, shuffle=true) #???
 
 hidden_width = 100
 model = Flux.Chain(
@@ -118,9 +128,9 @@ model = Flux.Chain(
 )
 opt = Flux.Adam(1e-4)
 
-supervised_model, supervised_test_loss_hist, res_sup, supervised_time = trainModelSupervised(deepcopy(model), deepcopy(opt), dataloader, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=1000)
-unsupervised_model, unsupervised_test_loss_hist, res_unsup, unsupervised_time = trainModelUnsupervised(deepcopy(model), deepcopy(opt), dataloader, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=1000)
-semisupervised_model, semisupervised_test_loss_hist, semisupervised_time = trainModelSemisupervised(deepcopy(model), deepcopy(opt), train_in, test_in, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; test_out=test_out, epochs=1000)
+supervised_model, supervised_test_loss_hist, res_sup, supervised_time = trainModelSupervised(deepcopy(model), deepcopy(opt), dataloader, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=100)
+unsupervised_model, unsupervised_test_loss_hist, res_unsup, unsupervised_time = trainModelUnsupervised(deepcopy(model), deepcopy(opt), dataloader, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=100)
+semisupervised_model, semisupervised_test_loss_hist, semisupervised_time = trainModelSemisupervised(deepcopy(model), deepcopy(opt), train_in, test_in, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; test_out=test_out, epochs=100)
 
 
 # plot mse loss results
@@ -151,12 +161,18 @@ xlabel!("x (x=r*s+b-y)")
 ylabel!("y prediction")
 
 
+# ⟹ supervised cant converge on ambigous data
+# ⟹ unsupervised can
+# ⟹ semi-supervised needs tuning to converge or, cant converge
+# ⟹ loss values can be misleading
+
+
 
 
 # 3. compare training time between all methods when using fully unsupervised training and fully supervised training (no clustering)
 (train_in, train_out, test_in, test_out) = readData(fileName, nInputs)
 train_in, train_out, test_in, test_out, train_in_t, train_out_t, test_in_t, test_out_t = scale_data_uniform(train_in, train_out, test_in, test_out)
-dataloader = Flux.DataLoader((train_in, train_out), batchsize=8, shuffle=true)
+dataloader = Flux.DataLoader((train_in, train_out), batchsize=1, shuffle=true)
 
 hidden_width = 100
 model = Flux.Chain(
@@ -166,13 +182,10 @@ model = Flux.Chain(
 )
 opt = Flux.Adam(1e-4)
 
-# two pipelines:
 epoch_range = [10,100,1000]
-# 1. fully unsupervised
-# 1.1. generate unsupervised data (or have a time how long it takes)
+
+
 gen_unsupervised_data_time = 0.05 # seconds for 1000 datapoints
-# 1.2. prepare unsupervised data
-# 1.3. train for [10,100,1000] epochs and plot training time and final test mse
 full_unsupervised_test_loss_hist = []
 full_res_unsupervised_test_loss_hist = []
 full_unsupervised_train_time_hist = []
@@ -185,11 +198,8 @@ for n_epochs in epoch_range
     push!(full_unsupervised_train_time_hist, unsupervised_time)
 end
 
-# 2. fully supervised
-# 2.1. generate supervised data (or have a time how long it takes)
+
 gen_supervised_data_time = 25 # seconds for 1000 datapoints
-# 2.2. prepare supervised data (no clustering)
-# 2.3. train for [10,100,1000] epochs and plot training time and final test mse
 full_supervised_test_loss_hist = []
 full_res_supervised_test_loss_hist = []
 full_supervised_train_time_hist = []
@@ -202,13 +212,16 @@ for n_epochs in epoch_range
     push!(full_supervised_train_time_hist, supervised_time)
 end
 
-# plot the test loss at the end of each run for both approaches
+
+# plot the mse test loss at the end of each run for both approaches
 plot_loss_history(full_unsupervised_test_loss_hist, epoch_range; label="unsupervised", marker = :circle)
 plot_loss_history!(full_supervised_test_loss_hist, epoch_range; label="supervised", marker = :circle)
 title!("final MSE for different approaches")
 xlabel!("Number of Epochs")
 ylabel!("MSE")
 
+
+# plot the residual test loss at the end of each run for both approaches
 plot_loss_history(full_res_unsupervised_test_loss_hist, epoch_range; label="unsupervised_res", marker = :circle)
 plot_loss_history!(full_res_supervised_test_loss_hist, epoch_range; label="supervised_res", marker = :circle)
 title!("final Residual for different approaches")
@@ -217,11 +230,18 @@ ylabel!("Residual")
 
 
 # plot the training time at the end of each run for both approaches
-plot_loss_history(full_unsupervised_train_time_hist, epoch_range; label="unsupervised", marker = :circle)
-plot_loss_history!(full_supervised_train_time_hist, epoch_range; label="supervised", marker = :circle)
+# with addition of time it takes to generate the data
+plot_loss_history(full_unsupervised_train_time_hist .+ gen_unsupervised_data_time, epoch_range; label="unsupervised", marker = :circle)
+plot_loss_history!(full_supervised_train_time_hist .+ gen_supervised_data_time, epoch_range; label="supervised", marker = :circle)
 title!("training time for different approaches")
 xlabel!("Number of Epochs")
 ylabel!("training time/s")
+
+
+# ⟹ training time increases linearly with number of epochs
+# ⟹ unsupervised data generation is ~500 times faster than supervised data generation
+
+
 
 # 4. unsupervised batchsize comparison 1 vs. 8
 (train_in, train_out, test_in, test_out) = readData(fileName, nInputs)
@@ -238,12 +258,26 @@ model = Flux.Chain(
 opt = Flux.Adam(1e-4)
 
 unsupervised_model_1, unsupervised_test_loss_hist_1, res_unsupervised_test_loss_hist_1,  unsupervised_time_1 = trainModelUnsupervised(
-    deepcopy(model), deepcopy(opt), dataloader_1, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=1000
+    deepcopy(model), deepcopy(opt), dataloader_1, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=100
     )
 
 unsupervised_model_8, unsupervised_test_loss_hist_8, res_unsupervised_test_loss_hist_8,  unsupervised_time_8 = trainModelUnsupervised(
-  deepcopy(model), deepcopy(opt), dataloader_8, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=1000
+  deepcopy(model), deepcopy(opt), dataloader_8, test_in, test_out, train_in_t, test_in_t, train_out_t, test_out_t, eq_num, sys_num, row_value_reference, fmu; epochs=100
   )
+
+
+plot_loss_history(unsupervised_test_loss_hist_1; label="unsupervised_1")
+plot_loss_history!(unsupervised_test_loss_hist_8; label="unsupervised_8")
+title!("MSE loss for different batchsizes")
+xlabel!("Number of Epochs")
+ylabel!("MSE")
+
+plot_loss_history(res_unsupervised_test_loss_hist_1; label="unsupervised_1")
+plot_loss_history!(res_unsupervised_test_loss_hist_8; label="unsupervised_8")
+title!("Residual loss for different batchsizes")
+xlabel!("Number of Epochs")
+ylabel!("Residual")
+
 
 
 test_in_rec = StatsBase.reconstruct(test_in_t, test_in)
@@ -254,6 +288,12 @@ plot_xy(unsupervised_model_8, test_in, test_out, test_in_t, test_out_t; label="u
 title!("XY plot for unclustered dataset")
 xlabel!("x (x=r*s+b-y)")
 ylabel!("y prediction")
+
+
+# ⟹ unsupervised with batchsize 1 converges
+# ⟹ larger batchsizes e.g. 8 lead to accuracy issues
+# ⟹ larger batchsizes train faster because of vectorization
+
 
 
 
